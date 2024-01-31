a714f3db56b7483f41aeea5a8da50e0c0b30e680
joaorocha <joaoeduardorrocha@gmail.com>
2024-01-31 12:58:48 -0300

    feat: Change Position Elements On HTML and Change Styles Positions

--- src/Components/Section/index.tsx
+++ src/Components/Section/index.tsx
@@ -10,30 +10,67 @@ const Section: React.FC = () => {
 
       <div className='feature'>
         <div className="feature-item">
-          <img 
-            className="feature-item__image"
-            src="https://64.media.tumblr.com/fffb7ac39a48612787f1d0c1398c4c9b/479adf28ff0a46db-63/s250x400/86c7e053a9158f2156b6fca6b583298fac0aa204.pnj" 
-            alt="Número 1 com um adorno roxo" 
+          <div className="feature-item-content">
+            <div className='feature-item-container'>
+              <img
+                className='feature-image'
+                src="https://64.media.tumblr.com/fffb7ac39a48612787f1d0c1398c4c9b/479adf28ff0a46db-63/s250x400/86c7e053a9158f2156b6fca6b583298fac0aa204.pnj" 
+                alt="" 
+                />
+              <h1 className="feature-item__title">Mala Direto Eficaz</h1>
+            </div>
+
+            <p className="feature-item__description">
+              Folder de duas dobras elaborado para chamar a atenção de possíveis clientes, despertando neles a curiosidade de conhecer melhor a sua empresa.
+            </p>
+            
+            <div className='feature-item-container'>
+              <img
+                className='feature-image'
+                src="https://64.media.tumblr.com/fffb7ac39a48612787f1d0c1398c4c9b/479adf28ff0a46db-63/s250x400/86c7e053a9158f2156b6fca6b583298fac0aa204.pnj" 
+                alt="" 
+                />
+              <h1 className="feature-item__title">Mala Direto Eficaz</h1>
+            </div>
+
+            <p className="feature-item__description">
+              Folder de duas dobras elaborado para chamar a atenção de possíveis clientes, despertando neles a curiosidade de conhecer melhor a sua empresa.
+            </p>
+            
+            <div className='feature-item-container'>
+              <img
+                className='feature-image'
+                src="https://64.media.tumblr.com/fffb7ac39a48612787f1d0c1398c4c9b/479adf28ff0a46db-63/s250x400/86c7e053a9158f2156b6fca6b583298fac0aa204.pnj" 
+                alt="" 
+                />
+              <h1 className="feature-item__title">Mala Direto Eficaz</h1>
+            </div>
+
+            <p className="feature-item__description">
+              Folder de duas dobras elaborado para chamar a atenção de possíveis clientes, despertando neles a curiosidade de conhecer melhor a sua empresa.
+            </p>
+            
+            <div className='feature-item-container'>
+              <img
+                className='feature-image'
+                src="https://64.media.tumblr.com/fffb7ac39a48612787f1d0c1398c4c9b/479adf28ff0a46db-63/s250x400/86c7e053a9158f2156b6fca6b583298fac0aa204.pnj" 
+                alt="" 
+                />
+              <h1 className="feature-item__title">Mala Direto Eficaz</h1>
+            </div>
+
+            <p className="feature-item__description">
+              Folder de duas dobras elaborado para chamar a atenção de possíveis clientes, despertando neles a curiosidade de conhecer melhor a sua empresa.
+            </p>
+          </div>
+
+          <img
+            className='feature-hero-image'
+            src="https://64.media.tumblr.com/19f38b1049af6528948c9a48ad05c74c/969d25c83a3fa691-52/s1280x1920/53d3f7847ea7b710df061ac1f263886c097907e5.pnj" 
+            alt="" 
           />
         </div>
-        
-        <div className="feature-item-2">
-          <h1 className="feature-item-2__title">Mala Direto Eficaz</h1>
-          <p className="feature-item-2__description">
-            Folder de duas dobras elaborado para chamar a atenção de possíveis clientes, despertando neles a curiosidade de conhecer melhor a sua empresa.
-          </p>
-        </div>
-
-        <img
-          className='hero-image'
-          src="https://64.media.tumblr.com/19f38b1049af6528948c9a48ad05c74c/969d25c83a3fa691-52/s1280x1920/53d3f7847ea7b710df061ac1f263886c097907e5.pnj" 
-          alt="" 
-        />
       </div>
-
-      
-
-
     </section>
   );
 };
