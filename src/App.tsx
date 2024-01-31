dc7436f967ddfb0605a776f9162235146dee5049
joaorocha <joaoeduardorrocha@gmail.com>
2024-01-31 11:07:18 -0300

    feat: Adding More Font Weights

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Section from './Components/Section'
 
 function App() {
   return (
     <div className="App">
-      
+      <Section />
     </div>
   );
 }
---
+++ src/Components/Section/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./index.css";
+
+const Section: React.FC = () => {
+  return (
+    <section className="section-content">
+      <h1 className="section-content__h1">
+        Por que você deveria nos escolher para sua melhor experiência
+      </h1>
+
+      
+    </section>
+  );
+};
+
+export default Section;
