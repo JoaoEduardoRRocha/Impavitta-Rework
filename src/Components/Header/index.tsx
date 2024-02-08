d95af5f2bf4fc7fde8a054657b6bf49d27e7f273
joaorocha <joaoeduardorrocha@gmail.com>
2024-02-08 18:31:48 -0300

    feature: Add Header Content

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -5,22 +5,41 @@ const Header: React.FC = () => {
   return (
     <header className='header-container'>
       <nav className='nav-container'>
-        <div className='nav-container__logo'>IMPÁVITTA</div>
+        <img
+          className='nav-container__logo'
+          src='https://64.media.tumblr.com/a7deea1f18f6efd83b88b1516c640e3b/97346d27a726b23c-00/s1280x1920/9f7259a744529f93194b727a996a50bf6e038931.pnj'
+          alt='Logotipo da Impavitta'
+        />
         <div className='nav-container__links'>
           <div>O que fazemos</div>
           <div>Sobre nós</div>
           <div>Contato</div>
         </div>
-
-        <div className='nav-text-container'>
-          <p className='nav-text-container__paragraph'>
-            Conquiste clientes com um material altamente profissional.
-          </p>
-          <p className='nav-text-container__paragraph-second'>
-            Saia na frente da concorrência!
-          </p>
-        </div>
       </nav>
+
+      <div className='nav-text-container'>
+        <p className='nav-text-container__paragraph'>
+          <strong>Conquiste</strong> clientes com um material altamente <strong>profissional.</strong>
+        </p>
+        <p className='nav-text-container__paragraph-second'>
+          Saia na frente da <strong>concorrência!</strong>
+        </p>
+      </div>
+
+      <div className='header-btn-container'>
+        <button className='header-btn-container__know-more'>
+          Saibe Mais
+        </button>
+        <button className='header-btn-container__contact-us'>
+          Fale Conosco
+        </button>
+      </div>
+
+      <div>
+        <p>
+          Profissionais com mais de 20 anos de experiência no mercado de administra ção do Rio de Janeiro e Niterói.
+        </p>
+      </div>
     </header>
   );
 };
